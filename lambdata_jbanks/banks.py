4ecf8ebe6b19dcba11d273c9a23ad7fdb4fee46c
Jennifer Banks <59672512+JenBanks8585@users.noreply.github.com>
2020-05-06 02:44:08 -0400

    added banks.py and mod.py

--- lambdata_jbanks/banks.py
+++ lambdata_jbanks/banks.py
@@ -1,5 +1,10 @@
 
 
+import pandas
+from pandas import DataFrame
+
+df=DataFrame({"a":[2,4,6], "b":[1,3,5]})
+print (df.head())
 
 
 print ("Hello")
---
+++ lambdata_jbanks/mod.py
@@ -0,0 +1,7 @@
+
+def enlarge(n):
+    return n*100
+
+
+x= int(input("enter an integer"))
+print(enlarge(x))
