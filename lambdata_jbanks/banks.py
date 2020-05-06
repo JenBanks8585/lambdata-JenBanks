f0cdc28408b1c0fedbcf19ccc17e3fbd8cba57b0
Jennifer Banks <59672512+JenBanks8585@users.noreply.github.com>
2020-05-06 09:50:34 -0400

    update banks by importing enlarge from mod

--- lambdata_jbanks/banks.py
+++ lambdata_jbanks/banks.py
@@ -2,7 +2,7 @@
 
 import pandas
 from pandas import DataFrame
-
+from lambdata_jbanks.mod import enlarge
 
 df=DataFrame({"a":[2,4,6], "b":[1,3,5]})
 print (df.head())
@@ -11,4 +11,4 @@ print (df.head())
 print ("Hello")
 
 x= 11
-#print(enlarge(x))
+print(enlarge(x))
